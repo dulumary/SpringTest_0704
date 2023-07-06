a0bb693227db7f08d566b5c50099669f67a6d511
asdf <asdf@gmail.com>
2023-07-06 21:02:07 +0900

    [김인규]  - jsp 연동 문제 풀이

--- src/main/java/com/marondal/spring/test/lifecycle/Test02Controller.java
+++ src/main/java/com/marondal/spring/test/lifecycle/Test02Controller.java
@@ -5,16 +5,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/lifecycle/test02")
 public class Test02Controller {
 	
 	
-	@RequestMapping("/lifecycle/test02/1")
-	@ResponseBody
+	@RequestMapping("/1")
 	public List<Map<String, Object>> movieList() {
 		
 		List<Map<String, Object>> movieList = new ArrayList<>();
@@ -46,11 +47,33 @@ public class Test02Controller {
 		
 	}
 	
-	public  postList() {
+	
+	@RequestMapping("/2")
+	public List<Post> postList() {
 		
 		List<Post> postList = new ArrayList<>();
 		Post post = new Post("안녕하세요 가입인사 드립니다", "hagulu", "안녕하세요 가입했어요. 앞으로 잘 부탁 드립니다.");
 		postList.add(post);
+		
+		post = new Post("헐 대박", "bada", "오늘 화요일이었어, 금요일인줄");
+		postList.add(post);
+		
+		post = new Post("오늘 데이트 한 이야기 해드릴게요", "dulumary", ".....");
+		postList.add(post);
+		
+		return postList;
+		
+	}
+	
+	@RequestMapping("/3")
+	public ResponseEntity<Post> postError() {
+		
+		Post post = new Post("안녕하세요 가입인사 드립니다", "hagulu", "안녕하세요 가입했어요. 앞으로 잘 부탁 드립니다.");
+		ResponseEntity<Post> entity = new ResponseEntity<>(post, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		
+		return entity;
+		
 	}
 	
 
