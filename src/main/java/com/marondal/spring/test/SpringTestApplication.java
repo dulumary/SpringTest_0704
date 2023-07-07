b47fb8e59fff2121fa342bc6238f796b6d788126
asdf <asdf@gmail.com>
2023-07-07 21:44:01 +0900

    [김인규]  - database 연동 문제 풀이

--- src/main/java/com/marondal/spring/test/SpringTestApplication.java
+++ src/main/java/com/marondal/spring/test/SpringTestApplication.java
@@ -6,11 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
 	}
+	
+	
 
 }
---
+++ src/main/java/com/marondal/spring/test/database/service/StoreService.java
@@ -0,0 +1,27 @@
+package com.marondal.spring.test.database.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marondal.spring.test.database.domain.Store;
+import com.marondal.spring.test.database.repository.StoreRepository;
+
+@Service
+public class StoreService {
+	
+	@Autowired
+	private StoreRepository storeRepository;
+	
+	
+	// store 리스트를 얻어 오는 기능
+	public List<Store> getStoreList() {
+		
+		List<Store> storeList = storeRepository.selectStoreList();
+		
+		return storeList;
+		
+	}
+
+}
