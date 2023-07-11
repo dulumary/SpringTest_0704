40c40ac74b18e9ace7477281a474d1fd4dde621d
asdf <asdf@gmail.com>
2023-07-11 21:33:17 +0900

    [김인규]  - mybatis insert update delete 문제

--- src/main/java/com/marondal/spring/test/mybatis/repository/RealEstateRepository.java
+++ src/main/java/com/marondal/spring/test/mybatis/repository/RealEstateRepository.java
@@ -20,5 +20,23 @@ public interface RealEstateRepository {
 	public List<RealEstate> selectRealEstateByAreaPrice(
 			@Param("area") int area
 			, @Param("price") int price);
-
+	
+	// RealEsate 객체를 기반으로 insert 수행
+	public int insertRealEstateByObject(RealEstate realEstate);
+	
+	public int insertRealEstate(
+			@Param("realtorId") int realtorId
+			, @Param("address") String address
+			, @Param("area") int area
+			, @Param("type") String type
+			, @Param("price") int price
+			, @Param("rentPrice") int rentPrice);
+	
+	public int updateRealEstate(
+			@Param("id") int id
+			, @Param("type") String type
+			, @Param("price") int price);
+	
+	public int deleteRealEstate(@Param("id") int id);
+	
 }
